f9ef88ebff743b59fe98d511f5de064090b090ac
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-12 15:59:42 +0600

    Js Task1 Update

--- Conceptual Session _ Problem Solving_Milestone-4/problem1.js
+++ Conceptual Session _ Problem Solving_Milestone-4/problem1.js
@@ -8,11 +8,14 @@ function cashOut(money) {
   if (money < 0 || typeof money !== "number") {
     return '"Invalid"';
   } else {
-    return money * (1.75 / 100);
+    let charge = money * (1.75 / 100);
+    let fixedCharge = charge.toFixed(4);
+    let finalCharge = parseFloat(fixedCharge);
+    return finalCharge;
   }
 }
 console.log(cashOut(2000));
-console.log(cashOut(100).toFixed(2));
+console.log(cashOut(100));
 console.log(cashOut(999));
 console.log(cashOut(-999));
 console.log(cashOut(0));
